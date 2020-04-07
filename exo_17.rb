0fe4c99b92848758e3f7f23a7c7b1be7b3ac24ec
jillsou <nguetsopjills@gmail.com>
2020-04-07 23:13:53 +0200

    add new exo 17

--- exo_17.rb
+++ exo_17.rb
@@ -1,14 +1,17 @@
 puts "Quel age avez-vous ?"
 age = gets.chomp.to_i
 i = 1
-b = age-1
-while age >= 1 
- puts "Il y a #{b} ans, tu avais #{i} ans"
+b = age
+while age != 1 
+
+ puts "Il y a #{age-1} ans, tu avais #{i} ans"
  age -= 1
  i += 1
- if b =i
-  puts "Il y a #{age/2} tu avais la moitié de ton age que tu as aujourd'hui"
+
+end
+
+ if age =i
+  puts "Il y a #{age/2} ans tu avais la moitié de ton age que tu as aujourd'hui"
    
- end
 end
 
